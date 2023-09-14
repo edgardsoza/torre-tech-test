41546079b661e257c000840d1982b27bff041348
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-09-14 11:17:40 -0600

    Fix linters

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import SearchBar from './components/SearchBar';
 function App() {
   return (
     <div className="App">
-        <div>
-          <SearchBar />
-        </div>
+      <div>
+        <SearchBar />
+      </div>
     </div>
   );
 }
--- src/components/FavoriteList.js
+++ src/components/FavoriteList.js
@@ -1,14 +1,14 @@
 // FavoriteList.js
-import React, { useState } from 'react';
+import React from 'react';
 
-const FavoriteList = ({ favorites, addToFavorites, removeFromFavorites }) => {
+const FavoriteList = ({ favorites, removeFromFavorites }) => {
   return (
-    <div className='favorite-container'>
+    <div className="favorite-container">
       <h1>Favorites</h1>
       <ul className='favorite-list'>
         {favorites.map((favorite) => (
           <li className="favorite-item" key={favorite.ggId}>
-            <a className='favorite-name' href={`https://torre.ai/${favorite.username}`} target="_blank" rel="noopener noreferrer">
+            <a className="favorite-name" href={`https://torre.ai/${favorite.username}`} target="_blank" rel="noopener noreferrer">
               <p>{favorite.name}</p>
             </a>
             <button className="remove-button" onClick={() => removeFromFavorites(favorite)}>Remove</button>
